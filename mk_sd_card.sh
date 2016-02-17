be4e1a579aa19ec1ca69b1db84678f684bc51800
Pascal Huerst <pascal.huerst@gmail.com>
2016-02-17 17:18:14 +0100

    libnlemphase: libnlaudiocontrol: Removed
    Since we have framebuffer now, we don't need these
    libs anymore

--- mk_sd_card.sh
+++ mk_sd_card.sh
@@ -3,7 +3,10 @@
 MOUNT_POINT_ROOT=/tmp/root
 MOUNT_POINT_BOOT=/tmp/boot
 IMAGE_VERSION_INFO_FILE=${MOUNT_POINT_ROOT}/etc/nonlinear_release
-SFDISK="output/host/usr/sbin/sfdisk"
+DEVICE="" # Set by $1
+DEVICE_P1=""
+DEVICE_P2=""
+
 
 function usage {
 	printf "Usage:\n\n"
@@ -20,6 +23,45 @@ function is_mounted {
 	fi
 }
 
+function write_release_file {
+	printf "Nonlinear Labs Nonlinux Version:\n" > ${IMAGE_VERSION_INFO_FILE}
+	printf "Nonlinux based on SHA1: $(git -C rev-parse HEAD)\n" >> ${IMAGE_VERSION_INFO_FILE}
+	printf "Image Built: $(date)\n" >> ${IMAGE_VERSION_INFO_FILE}
+	printf "Snapshot: $(git rev-parse --abbrev-ref HEAD)"
+}
+
+function check_if_mounted_and_unmount {
+	PARTITIONS=$(ls "$DEVICE"?* 2>/dev/null)
+	if [ -n "$PARTITIONS" ]; then
+		printf "Checking mountpoints:\n"
+	fi
+
+	for partition in $PARTITIONS; do
+		is_mounted $partition
+		if [ $? -eq 1 ]; then
+			printf "  $partition: is mounted. Unmounting...\n"
+			umount "$partition" 2>/dev/null
+		else
+			printf "  $partition: is not mounted. Ok...\n"
+		fi
+	done
+	printf "\n"
+}
+
+
+# usage:
+#  get_partition "/dev/sdc" "1"
+#  returns "/dev/sdc1"
+#  get_partition "/dev/mmcblk0" "2"
+#  returns "/dev/mmcblk0p2"
+function get_partition {
+[[ $1 == *"/dev/mmcblk"* ]] && echo "${1}p${2}"
+[[ $1 == *"/dev/sd"* ]] && echo "${1}${2}"
+}
+
+
+###############################################################################
+
 if [ "$(id -u)" != "0" ]; then
 	echo "This script must be run as root" 1>&2
 	exit -1
@@ -29,57 +71,46 @@ if [ -z "$1" ]; then
 	usage
 fi
 
-DEVICE=""
 if [ -b "$1" ]; then
 	DEVICE=$1
+	DEVICE_P1=$(get_partition $DEVICE 1)
+	DEVICE_P2=$(get_partition $DEVICE 2)
+	printf "Working on:\n"
+	printf "  $DEVICE\n"
+	printf "    $DEVICE_P1  boot\n"
+	printf "    $DEVICE_P2  rootfs\n\n"
 else
 	printf "Device $1 does not seem to be a block device!\n"
 	exit -1
 fi
 
-PARTITIONS=$(ls "$DEVICE"?* 2>/dev/null)
-if [ -n "$PARTITIONS" ]; then
-	printf "Checking mountpoints:\n"
-fi
-
-for partition in $PARTITIONS; do
-	is_mounted $partition
-	if [ $? -eq 1 ]; then
-		printf "  $partition: is mounted. Unmounting...\n"
-		umount "$partition" 2>/dev/null
-	else
-		printf "  $partition: is not mounted. Ok...\n"
-	fi
-done
+check_if_mounted_and_unmount
 
 printf "Flushing old partition table...\n"
 dd if=/dev/zero of=$DEVICE bs=1024 count=1024 2>/dev/null 1>/dev/null && sync
 
 printf "Creating new partition table...\n"
-echo -e ',50M,c,*\n,\n' | sudo $SFDISK $DEVICE 2>/dev/null 1>/dev/null && sync
+echo -e ',50M,c,*\n,\n' | sudo sfdisk $DEVICE 2>/dev/null 1>/dev/null && sync
 
 printf "Creating Partitions...\n"
-mkfs.vfat -n BOOT ${DEVICE}1 1>/dev/null 2>/dev/null
-mkfs.ext3 ${DEVICE}2 1>/dev/null 2>/dev/null
+mkfs.vfat -n BOOT ${DEVICE_P1} 1>/dev/null 2>/dev/null
+mkfs.ext3 ${DEVICE_P2} 1>/dev/null 2>/dev/null
 
 printf "Rereading partition table...\n"
 partprobe $DEVICE && sync
 
 printf "Mounting at rootfs at $MOUNT_POINT_ROOT\n"
 mkdir -p ${MOUNT_POINT_ROOT}
-mount ${DEVICE}2 ${MOUNT_POINT_ROOT}
+mount ${DEVICE_P2} ${MOUNT_POINT_ROOT}
 printf "Copy rootfs to target...\n"
-tar -C ${MOUNT_POINT_ROOT} -xf output/images/rootfs.tar && sync
+pv output/images/rootfs.tar | tar -C ${MOUNT_POINT_ROOT} -xf && sync
 printf "Updating build information in target fs...\n"
-echo "Nonlinear Labs Nonlinux Version:" > ${IMAGE_VERSION_INFO_FILE}
-echo "Nonlinux based on SHA1: $(git -C rev-parse HEAD)" >> ${IMAGE_VERSION_INFO_FILE}
-echo "Image Built: $(date)" >> ${IMAGE_VERSION_INFO_FILE}
 printf "Unmounting rootfs...\n"
 umount ${MOUNT_POINT_ROOT}
 
 printf "Mounting at boot at $MOUNT_POINT_BOOT\n"
 mkdir -p ${MOUNT_POINT_BOOT}
-mount ${DEVICE}1 ${MOUNT_POINT_BOOT}
+mount ${DEVICE_P1} ${MOUNT_POINT_BOOT}
 cp -v output/images/u-boot.img ${MOUNT_POINT_BOOT} && sync
 cp -v output/images/MLO ${MOUNT_POINT_BOOT} && sync
 printf "Creating uEnv.txt...\n"
