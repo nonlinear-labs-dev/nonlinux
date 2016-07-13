366dceadd5f41c5dd6f290c00ec03efbbdcc5b37
buildserver <buildserver@localhost.localdomain>
2016-07-13 14:57:00 +0200

    postbuild-scripts: Beautified the output

--- nonlinear/scripts/jobs/generate-machine-id.sh
+++ nonlinear/scripts/jobs/generate-machine-id.sh
@@ -4,7 +4,7 @@ OUTFILE=$WD/etc/machine-id
 
 MACHINE_ID=`< /dev/urandom tr -dc '[:xdigit:]' | head -c32 | tr '[:upper:]' '[:lower:]'; echo;`;
 
-echo Generated /etc/machine_id = [$MACHINE_ID]
+echo "  Generated /etc/machine_id = [$MACHINE_ID]"
 echo $MACHINE_ID > $OUTFILE
 
 exit 0
