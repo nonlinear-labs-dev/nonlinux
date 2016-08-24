1b755878f4a66f87ad35db3dbd1888497cee0ccd
buildserver <buildserver@localhost.localdomain>
2016-08-24 17:17:40 +0200

    postbuild-scripts: make sure /var/log is a folder

--- nonlinear/scripts/jobs/remove-unwanted-files.sh
+++ nonlinear/scripts/jobs/remove-unwanted-files.sh
@@ -5,6 +5,8 @@ echo "  Remove unwanted files..."
 
 # We don't want dhcpd to start by default. We start it by udev rule if needed
 rm -f ${WD}/etc/systemd/system/multi-user.target.wants/dhcpd.service
-
+# It does not always work te replace /avr/log (which is a symlink) by a folder
+rm -Rf ${WD}/var/log
+mkdir -p ${WD}/var/log
 
 exit 0
