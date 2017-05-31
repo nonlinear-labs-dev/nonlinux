c934c740fb171670eb4bc6ade8725f55ccc2b279
Henry <henry@hoegelow.com>
2017-05-31 16:30:37 +0200

    mxli flags corrupted compile flags for glib

--- package/nonlinear/mxli/mxli.mk
+++ package/nonlinear/mxli/mxli.mk
@@ -10,12 +10,9 @@ MXLI_SITE = http://www.windscooting.com/softy
 MXLI_TARGET_DIR = $(TARGET_DIR)/nonlinear/scripts/mxli
 MXLI_INSTALL_TARGET = YES
 
-TARGET_CFLAGS+="-I$(@D)/lib/c-linux\ -I$(@D)/lib\ -I$(@D)/lib/c-any"
-TARGET_LDFLAGS+="-L$(@D)/lib/c-linux\ -L$(@D)/lib\ -L$(@D)/lib/c-any"
-
 define MXLI_BUILD_CMDS
-	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)
-	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/programs/mxli3
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) LDFLAGS="-L$(@D)/lib/c-linux -L$(@D)/lib -L$(@D)/lib/c-any" CFLAGS="-I$(@D)/lib/c-linux -I$(@D)/lib -I$(@D)/lib/c-any" -C $(@D)
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) LDFLAGS="-L$(@D)/lib/c-linux -L$(@D)/lib -L$(@D)/lib/c-any" CFLAGS="-I$(@D)/lib/c-linux -I$(@D)/lib -I$(@D)/lib/c-any" -C $(@D)/programs/mxli3
 endef
 
 define MXLI_INSTALL_TARGET_CMDS
