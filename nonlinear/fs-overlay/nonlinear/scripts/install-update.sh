cb5a7c30972a58c0e254eb04e05d09e036676c44
Henry <henry@hoegelow.com>
2017-05-31 10:59:06 +0200

    simplified update scripts and added factory image target

--- nonlinear/fs-overlay/nonlinear/scripts/install-update.sh
+++ nonlinear/fs-overlay/nonlinear/scripts/install-update.sh
@@ -6,6 +6,9 @@ if [ -e /mnt/usb-stick/nonlinear-c15-update.tar ]
 then
 	cp /mnt/usb-stick/nonlinear-c15-update.tar /update		
 	mv /mnt/usb-stick/nonlinear-c15-update.tar /mnt/usb-stick/nonlinear-c15-update.tar-copied
-	/bin/sh /nonlinear/scripts/unpack-and-install-update.sh
+	cd /update
+	tar xvf nonlinear-c15-update.tar
+	chmod +x /update/run.sh
+	/bin/sh /update/run.sh
 fi
 
