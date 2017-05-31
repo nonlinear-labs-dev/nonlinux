0fe909b9f07701aa6b69c4633076bd4a0e4fb0fc
Henry <henry@hoegelow.com>
2017-05-31 10:59:06 +0200

    simplified update scripts and added factory image target

--- nonlinear/fs-overlay/nonlinear/scripts/unpack-and-install-update.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-cd /update
-tar xvf nonlinear-c15-update.tar
-/bin/sh run.sh
--- package/nonlinear/mxli/mxli.mk
+++ package/nonlinear/mxli/mxli.mk
@@ -8,7 +8,6 @@ MXLI_VERSION = 3.1
 MXLI_SOURCE = c-linux-mxli-3.1.tar.bz2
 MXLI_SITE = http://www.windscooting.com/softy
 MXLI_TARGET_DIR = $(TARGET_DIR)/nonlinear/scripts/mxli
-MXLI_DEPENDENCIES = playground
 MXLI_INSTALL_TARGET = YES
 
 TARGET_CFLAGS+="-I$(@D)/lib/c-linux\ -I$(@D)/lib\ -I$(@D)/lib/c-any"
