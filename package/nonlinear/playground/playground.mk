8fd01855816859d47dda7a4f728f98dc0e8e3aa9
Henry Hoegelow <henry@hoegelow.com>
2019-11-14 10:50:07 +0100

    Update playground.mk
    bind playground to Release 2019/05

--- package/nonlinear/playground/playground.mk
+++ package/nonlinear/playground/playground.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PLAYGROUND_VERSION = HEAD
+PLAYGROUND_VERSION = RELEASE-2019-05
 PLAYGROUND_SITE = https://github.com/nonlinear-labs-dev/C15.git
 PLAYGROUND_SITE_METHOD = git
 PLAYGROUND_LICENSE = GPLv3+
