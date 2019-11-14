e1941b0baffe3bb6344c7ed58d93eebe5f8b2b5c
Henry Hoegelow <henry@hoegelow.com>
2019-11-14 13:12:32 +0100

    Update playground.mk

--- package/nonlinear/playground/playground.mk
+++ package/nonlinear/playground/playground.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PLAYGROUND_VERSION = RELEASE-2019-05
+PLAYGROUND_VERSION = HEAD
 PLAYGROUND_SITE = https://github.com/nonlinear-labs-dev/C15.git
 PLAYGROUND_SITE_METHOD = git
 PLAYGROUND_LICENSE = GPLv3+
@@ -13,6 +13,6 @@ PLAYGROUND_DEPENDENCIES = util-linux glibmm libsoup avahi dbus host-gwt
 PLAYGROUND_TARGET_DIR = $(TARGET_DIR)/nonlinear/playground
 PLAYGROUND_SOURCE_DIR = $(PLAYGROUND_DIR)
 PLAYGROUND_INSTALL_TARGET = YES
-PLAYGROUND_CONF_OPTS += -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=/nonlinear/playground -DJARSDIR=$(GWT_COMPILER_DIR)
+PLAYGROUND_CONF_OPTS += -DBUILD_AUDIOENGINE=Off -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=/nonlinear/playground -DJARSDIR=$(GWT_COMPILER_DIR)
 
 $(eval $(cmake-package))
