f2d59688bcfe63ef666ada4a7da118488bada6df
Henry Hoegelow <henry@hoegelow.com>
2019-12-19 09:45:22 +0100

    rename project playground into C15

---
+++ package/nonlinear/C15/C15.mk
@@ -0,0 +1,18 @@
+################################################################################
+#
+# C15
+#
+################################################################################
+
+C15_VERSION = HEAD
+C15_SITE = https://github.com/nonlinear-labs-dev/C15.git
+C15_SITE_METHOD = git
+C15_LICENSE = GPLv3+
+C15_LICENSE_FILES = COPYING
+C15_DEPENDENCIES = util-linux glibmm libsoup avahi dbus host-gwt
+C15_SOURCE_DIR = $(C15_DIR)
+C15_INSTALL_TARGET = YES
+C15_CONF_OPTS += -DBUILD_AUDIOENGINE=Off -DCMAKE_BUILD_TYPE=Release -DJARSDIR=$(GWT_COMPILER_DIR)
+C15_SUPPORTS_IN_SOURCE_BUILD = No
+
+$(eval $(cmake-package))
