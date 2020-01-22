82b3bf75c67e69b7c17c5ece69c5ba3191b132b9
Henry Hoegelow <henry@hoegelow.com>
2020-01-22 09:32:07 +0100

    fixes integration with new build tools

--- package/nonlinear/C15/C15.mk
+++ package/nonlinear/C15/C15.mk
@@ -5,14 +5,14 @@
 ################################################################################
 
 C15_VERSION = master
-C15_SITE = https://github.com/nonlinear-labs-dev/C15.git
-C15_SITE_METHOD = git
+C15_SITE = /sources/projects
+C15_SITE_METHOD = local
 C15_LICENSE = GPLv3+
 C15_LICENSE_FILES = COPYING
 C15_DEPENDENCIES = util-linux glibmm libsoup boost
 C15_SOURCE_DIR = $(C15_DIR)
 C15_INSTALL_TARGET = YES
-C15_CONF_OPTS += -DBUILD_AUDIOENGINE=Off -DBUILD_PLAYGROUND=Off -DCMAKE_BUILD_TYPE=Release -DJARSDIR=$(GWT_COMPILER_DIR)
+C15_CONF_OPTS += -DBUILD_AUDIOENGINE=Off -DBUILD_PLAYGROUND=Off -DBUILD_ONLINEHELP=Off -DCMAKE_BUILD_TYPE=Release -DJARSDIR=$(GWT_COMPILER_DIR)
 C15_SUPPORTS_IN_SOURCE_BUILD = No
 
 $(eval $(cmake-package))
