429ec5692665c1c79a71ba2b6a404a5b1b2c6b5e
Henry Hoegelow <henry@hoegelow.com>
2020-03-05 23:16:54 +0100

    more clever local rsync update

--- package/nonlinear/C15/C15.mk
+++ package/nonlinear/C15/C15.mk
@@ -5,8 +5,15 @@
 ################################################################################
 
 C15_VERSION = master
-C15_SITE = /sources/projects
+C15_SITE = /sources
 C15_SITE_METHOD = local
+
+define DELETE_ARTIFACTS
+	rsync --delete -avu --chmod=u=rwX,go=rX --exclude .svn --exclude .git --exclude .hg --exclude .bzr --exclude CVS -f 'P buildroot-build/' $(C15_SITE)/ $(@D)
+endef
+
+C15_POST_RSYNC_HOOKS += DELETE_ARTIFACTS
+
 C15_LICENSE = GPLv3+
 C15_LICENSE_FILES = COPYING
 C15_DEPENDENCIES = util-linux glibmm libsoup boost systemd freetype
