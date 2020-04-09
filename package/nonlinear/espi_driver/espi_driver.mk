8c2086733392810ec2573dbf76bce3ec68bd439e
Henry <henry@hoegelow.com>
2020-04-09 15:50:44 +0200

    use nl driver sources from local c15 repo instead of github repos

--- package/nonlinear/espi_driver/espi_driver.mk
+++ package/nonlinear/espi_driver/espi_driver.mk
@@ -7,11 +7,17 @@
 ESPI_DRIVER_MODULE = espi_driver
 ESPI_DRIVER_INSTALL_STAGING = YES
 ESPI_DRIVER_VERSION = master
-ESPI_DRIVER_SITE = https://github.com/nonlinear-labs-dev/espi.git
-ESPI_DRIVER_SITE_METHOD = git
+ESPI_DRIVER_SITE = /sources/projects/bbb-espi-driver
+ESPI_DRIVER_SITE_METHOD = local
 ESPI_DRIVER_DEPENDENCIES = linux
 ESPI_DRIVER_LICENSE = GPLv3+
 ESPI_DRIVER_LICENSE_FILES = COPYING
+
+define ESPI_DRIVER_DELETE_ARTIFACTS
+    rsync --delete -avu --chmod=u=rwX,go=rX --exclude .svn --exclude .git --exclude .hg --exclude .bzr --exclude CVS -f 'P buildroot-build/' $(ESPI_DRIVER_SITE)/ $(@D)
+endef
+
+ESPI_DRIVER_POST_RSYNC_HOOKS += ESPI_DRIVER_DELETE_ARTIFACTS
 
 define ESPI_DRIVER_BUILD_CMDS
   $(MAKE) $(LINUX_MAKE_FLAGS) -C $(LINUX_DIR) M=$(@D) modules
@@ -22,3 +28,4 @@ define ESPI_DRIVER_INSTALL_TARGET_CMDS
 endef
 
 $(eval $(generic-package))
+
--- package/nonlinear/lpc_bb_driver/lpc_bb_driver.mk
+++ package/nonlinear/lpc_bb_driver/lpc_bb_driver.mk
@@ -7,12 +7,15 @@
 LPC_BB_DRIVER_MODULE = lpc_bb_driver
 LPC_BB_DRIVER_INSTALL_STAGING = YES
 LPC_BB_DRIVER_VERSION = master
-LPC_BB_DRIVER_SITE = https://github.com/nonlinear-labs-dev/lpc_bb.git
-LPC_BB_DRIVER_SITE_METHOD = git
+LPC_BB_DRIVER_SITE = /sources/projects/bbb-lpc-driver
+LPC_BB_DRIVER_SITE_METHOD = local
 LPC_BB_DRIVER_DEPENDENCIES = linux
-LPC_BB_DRIVER_LICENSE = GPLv3+
-LPC_BB_DRIVER_LICENSE_FILES = COPYING
 
+define LPC_BB_DRIVER_DELETE_ARTIFACTS
+    rsync --delete -avu --chmod=u=rwX,go=rX --exclude .svn --exclude .git --exclude .hg --exclude .bzr --exclude CVS -f 'P buildroot-build/' $(LPC_BB_DRIVER_SITE)/ $(@D)
+endef
+
+LPC_BB_DRIVER_POST_RSYNC_HOOKS += LPC_BB_DRIVER_DELETE_ARTIFACTS
 
 define LPC_BB_DRIVER_BUILD_CMDS
   $(MAKE) $(LINUX_MAKE_FLAGS) -C $(LINUX_DIR) M=$(@D) modules
