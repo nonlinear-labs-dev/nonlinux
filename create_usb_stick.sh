a21e3671614827c54c55c8185d3dc1eb25057d7d
AntonSchmied <37212595+AntonSchmied@users.noreply.github.com>
2021-01-13 09:58:57 +0100

    Update create_usb_stick.sh

--- create_usb_stick.sh
+++ create_usb_stick.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+!/bin/bash
 
 # THIS MUST POINT TO BUILT BUILDROOT
 SOURCES=$(readlink -f ../nonlinux)
@@ -8,11 +8,11 @@ BASEDIR=$(dirname "${0}")
 
 make -C "${BASEDIR}" host-nlimagemaker
 
-NLIMAGEMAKER=`readlink -f "${BASEDIR}/output/host/usr/sbin/nlimagemaker"`
+NLIMAGEMAKER=`readlink -f "${BASEDIR}/output/host/usr/bin/nlimagemaker"`
 
 echo ${NLIMAGEMAKER}
 echo ${SOURCES}
 
-${NLIMAGEMAKER} -k ${BASEDIR}/output/images/uImage -r ${SOURCES}/output/images/rootfs.tar.gz -d ${BASEDIR}/output/images/am335x-boneblack.dtb
+${NLIMAGEMAKER} -k ${BASEDIR}/output/images/uImage -r ${SOURCES}/output/images/rootfs.tar -d ${BASEDIR}/output/images/am335x-boneblack.dtb -o /tmp/nonlinear.img
 
 echo "Image created: /tmp/nonlinear.img"
