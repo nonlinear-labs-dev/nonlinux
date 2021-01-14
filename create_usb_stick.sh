356b7e2b9f8b3e80e5639c607ebac2a8b22954a5
AntonSchmied <anton.schmied@hotmail.de>
2021-01-14 16:13:33 +0000

    fixes

--- create_usb_stick.sh
+++ create_usb_stick.sh
@@ -11,7 +11,7 @@ NLIMAGEMAKER=`readlink -f "${BASEDIR}/output/host/usr/bin/nlimagemaker"`
 echo ${NLIMAGEMAKER}
 echo ${SOURCES}
 
-gzip -c ${SOURCES}/output/images/rootfs.tar ${SOURCES}/output/images/rootfs.tar.gz 
+gzip -c ${SOURCES}/output/images/rootfs.tar > ${SOURCES}/output/images/rootfs.tar.gz 
 ${NLIMAGEMAKER} -k ${BASEDIR}/output/images/uImage -r ${SOURCES}/output/images/rootfs.tar.gz -d ${BASEDIR}/output/images/am335x-boneblack.dtb -o /tmp/nonlinear.img
 
 echo "Image created: /tmp/nonlinear.img"
