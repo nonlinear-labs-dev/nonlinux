f7094a7b1fa9856122475012d24c82f3e1da0c1e
AntonSchmied <anton.schmied@hotmail.de>
2021-12-02 11:52:43 +0100

    typo fixes

--- nonlinear/fs-overlay-usb-os-restore/usr/bin/restore-c15-os.sh
+++ nonlinear/fs-overlay-usb-os-restore/usr/bin/restore-c15-os.sh
@@ -8,6 +8,9 @@ set -x
 
 LOG_FILE="/media/error.log"
 TIMEOUT=5
+BBB_DEVICE="/dev/mmcblk0p2"
+BBB_ROOTFS_MOUNTPOINT="/tmp/bbb_rootfs"
+BBB_ROOTFS_UPDATE_DIR="${BBB_ROOTFS_MOUNTPOINT}/update"
 
 t2s() {
     /media/utilities/text2soled multitext "$1" "$2" "$3" "$4" "$5" "$6"
@@ -53,39 +56,42 @@ check_preconditions (){
     return 0
 }
 
+mount_rootfs (){
+    if ( ! mount | grep ${BBB_DEVICE} ); then
+        mkdir ${BBB_ROOTFS_MOUNTPOINT} \
+        && mount ${BBB_DEVICE} ${BBB_ROOTFS_MOUNTPOINT} \
+        || { report "" "Could not mount local rootfs"; return 1; }
+    fi
+    return 0
+}
+
 unpack_update (){
-    mkdir /tmp/update \
-    && cp /media/nonlinear-c15-update.tar /tmp/update \
-    && cd /tmp/update \
+    rm -r ${BBB_ROOTFS_UPDATE_DIR}/* \
+    && cp /media/nonlinear-c15-update.tar ${BBB_ROOTFS_UPDATE_DIR} \
+    && cd ${BBB_ROOTFS_UPDATE_DIR} \
     && tar xf nonlinear-c15-update.tar \
     || { report "" "Could not unpack update"; return 1; }
 
-    mkdir /tmp/update/BBB/rootfs \
-    && gzip -dc /tmp/update/BBB/rootfs.tar.gz | tar -C /tmp/update/BBB/rootfs -xf - \
+    mkdir ${BBB_ROOTFS_UPDATE_DIR}/BBB/rootfs \
+    && gzip -dc ${BBB_ROOTFS_UPDATE_DIR}/BBB/rootfs.tar.gz | tar -C ${BBB_ROOTFS_UPDATE_DIR}/BBB/rootfs -xf - \
     || { report "" "Could not unpack rootfs"; return 1; }
 
     return 0
 }
 
-mount_rootfs (){
-    mkdir /tmp/bbb_rootfs && mount /dev/mmcblk0p2 /tmp/bbb_rootfs \
-    || { report "" "Could not mount local rootfs"; return 1; }
-
-    return 0
-}
 
 
 sync_rootfs (){
-    LD_LIBRARY_PATH=/media/utilities /media/utilities/rsync -cax --exclude '/tmp/bbb_rootfs/etc/hostapd.conf' /tmp/update/BBB/rootfs /tmp/bbb_rootfs \
+    LD_LIBRARY_PATH=/media/utilities /media/utilities/rsync -cax --exclude '${BBB_ROOTFS_MOUNTPOINT}/etc/hostapd.conf' --exclude '${BBB_ROOTFS_MOUNTPOINT}/update' ${BBB_ROOTFS_UPDATE_DIR}/BBB/rootfs ${BBB_ROOTFS_MOUNTPOINT} \
     || { report "" "Faled to sync new rootfs"; return 1; }
     return 0
 }
 
 main (){
     mount_stick
-    check_update_presence || return 1
-    unpack_update || return 1
+    check_preconditions || return 1
     mount_rootfs || return 1
+    unpack_update || return 1
 #    sync_rootfs || return 1
     return 0
 }
